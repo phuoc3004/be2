21dfd2d206d59001295617385d46228b829d285c
phuoc3004 <100799248+phuoc3004@users.noreply.github.com>
2023-12-25 11:11:29 +0700

    8nd commit

--- src/main/java/com/example/PBL6/controller/PaymentController.java
+++ src/main/java/com/example/PBL6/controller/PaymentController.java
@@ -137,7 +137,7 @@ public class PaymentController {
                     }
                 } else if (userAgent.contains("Mozilla")) {
                     HttpHeaders httpHeaders = new HttpHeaders();
-                    httpHeaders.add("location", "http://localhost:8080/payment/success");
+                    httpHeaders.add("location", "http://localhost:3000/payment/success");
                     if(orderRequest.getProductId() != null) {
                         orderService.saveOrderBuyNow(user, orderRequest, "COMPLETE","VNPAY");
                     } else {
@@ -149,7 +149,7 @@ public class PaymentController {
             } else {
                 if(userAgent.contains("Mozilla")) {
                     HttpHeaders httpHeaders = new HttpHeaders();
-                    httpHeaders.add("location", "http://localhost:8080/payment/fail");
+                    httpHeaders.add("location", "http://localhost:3000/payment/fail");
                     return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
                 }
                 paymentResultDto.setStatus("FAIL");
